5e8bb4d24eb9a598577a89d15bb34e60f49570a7
arisnotargon <715535007@qq.com>
2022-08-18 08:45:36 +0800

    1

--- 01_Introduction/02_Creating_and_Using_Tensors/02_tensors.py
+++ 01_Introduction/02_Creating_and_Using_Tensors/02_tensors.py
@@ -1,6 +1,6 @@
 # Tensors
-#----------------------------------
-#
+# ----------------------------------
+# test
 # This function introduces various ways to create
 # tensors in TensorFlow
 
@@ -11,16 +11,17 @@ ops.reset_default_graph()
 # Introduce tensors in tf
 
 # Get graph handle
-sess = tf.Session()
+# sess = tf.Session()
+sess = tf.compat.v1.Session()
 
-my_tensor = tf.zeros([1,20])
+my_tensor = tf.zeros([1, 20])
 
 # Declare a variable
-my_var = tf.Variable(tf.zeros([1,20]))
+my_var = tf.Variable(tf.zeros([1, 20]))
 
 # Different kinds of variables
 row_dim = 2
-col_dim = 3 
+col_dim = 3
 
 # Zero initialized variable
 zero_var = tf.Variable(tf.zeros([row_dim, col_dim]))
@@ -46,23 +47,29 @@ const_var = tf.Variable(tf.constant([8, 6, 7, 5, 3, 0, 9]))
 const_fill_var = tf.Variable(tf.constant(-1, shape=[row_dim, col_dim]))
 
 # Sequence generation
-linear_var = tf.Variable(tf.linspace(start=0.0, stop=1.0, num=3)) # Generates [0.0, 0.5, 1.0] includes the end
+# Generates [0.0, 0.5, 1.0] includes the end
+linear_var = tf.Variable(tf.linspace(start=0.0, stop=1.0, num=3))
 
-sequence_var = tf.Variable(tf.range(start=6, limit=15, delta=3)) # Generates [6, 9, 12] doesn't include the end
+# Generates [6, 9, 12] doesn't include the end
+sequence_var = tf.Variable(tf.range(start=6, limit=15, delta=3))
 
 # Random Numbers
 
 # Random Normal
-rnorm_var = tf.random_normal([row_dim, col_dim], mean=0.0, stddev=1.0)
+# rnorm_var = tf.random_normal([row_dim, col_dim], mean=0.0, stddev=1.0)
+rnorm_var = tf.random.normal([row_dim, col_dim], mean=0.0, stddev=1.0)
 
 # Add summaries to tensorboard
-merged = tf.summary.merge_all()
+# merged = tf.summary.merge_all()
+
+merged = tf.compat.v1.summary.merge_all()
 
 # Initialize graph writer:
-writer = tf.summary.FileWriter("/tmp/variable_logs", graph=sess.graph)
+# writer = tf.summary.FileWriter("/tmp/variable_logs", graph=sess.graph)
+writer = tf.compat.v1.summary.FileWriter("/tmp/variable_logs", graph=sess.graph)
 
 # Initialize operation
-initialize_op = tf.global_variables_initializer()
+initialize_op = tf.compat.v1.global_variables_initializer()
 
 # Run initialization of variable
-sess.run(initialize_op)
+print(sess.run(initialize_op))
---
+++ note.md
@@ -0,0 +1,4 @@
+# 学tensor的笔记
+
+## 数学基础视频
+https://www.bilibili.com/video/BV1ht411E79D
